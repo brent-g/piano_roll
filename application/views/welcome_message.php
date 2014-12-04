50438cc405fcf5a26c609b63085e99930f3109ba
brent-g <brent.godsoe@gmail.com>
2014-12-04 18:23:06 -0500

    Menu stuff

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,27 +1,6 @@
 	<style type="text/css">
-
-		/*::selection{ background-color: #E13300; color: white; }*/
-		/*::moz-selection{ background-color: #E13300; color: white; }*/
-		/*::webkit-selection{ background-color: #E13300; color: white; }*/
-
-/*	* {
-	  margin:0px;
-	  padding:0px;
-	  list-style:none;
-	}*/
-
 	:focus {
 	  outline:none !important;
-	}
-
-	body {
-/*	  background:#666;
-	  background:-webkit-radial-gradient(bottom left,cover,#999,#666);
-	  background:-moz-radial-gradient(bottom left,cover,#999,#666);
-	  background:-ms-radial-gradient(bottom left,cover,#999,#666);
-	  background:-o-radial-gradient(bottom left,cover,#999,#666);
-	  background:radial-gradient(bottom left,cover,#999,#666);*/
-	  height:500px;
 	}
 
 	a {
@@ -33,96 +12,35 @@
 	  text-decoration:underline;
 	}
 
-	
-	li {
-		/*color:red;*/
-	}
 	.selected {
 		background:#3399FF !important;
 	}
+	
 	span {
 		color:white;
 	}
+
 	#container {
 		overflow: auto;
 	}
-
 </style>
 <script>
+	// default values
 	var current_octave = 2;
 	var sound_volume = 1;
+	var keyboard_control = true;
 
 	$(function(){
-
 		
+		keyboard_init();
+		
+		// set the sound volume value according to the slider
 		$('.range-slider').on('change', function(){
 			var slider_value = $('.range-slider').attr('data-slider');
+			// set the sound volume value to a number between 0 and 1
 			sound_volume = (slider_value / 100);
 		});
 		
-		var key_down = {};
-		$(document).on({
-		    "keydown": function(e) 
-		    { 
-    			// Decrease Octave
-				if (e.keyCode === 90)
-				{
-					if (current_octave > 0)
-					{
-						current_octave--;
-						//$('#piano [octave|="'+current_octave+'"] li').css({"border-style": "solid 2px;","border-left-color":"blue"})
-					} 
-					else 
-					{
-						return false;
-					}
-				} 
-				
-				// Increase Octave
-				else if (e.keyCode === 88)
-				{
-					if (current_octave < 4)
-					{
-						current_octave++;
-					}
-					else
-					{
-						return false;
-					}
-				}
-				else 
-				{    			
-    				$(note).each(function(k,value) 
-    				{
-    					if(e.keyCode === value.key_code) 
-    					{
-    						if (key_down[value.key_code] == null) 
-    						{
-	    						var note_octave = current_octave + value.octave_diff;
-	    						$("#piano [octave|="+note_octave+"] li div[key|="+value.key+"]").addClass('selected');
-	    						$("#piano [octave|="+note_octave+"] li span[key|="+value.key+"]").addClass('selected');
-	    						play_multi_sound(note_octave+value.sound);
-	    						key_down[value.key_code] = true;
-    						}
-    					}
-    				});
-				}
-			},
-		    "keyup": function(e) 
-		    { 
-				$(note).each(function(k,value) 
-				{
-					if(e.keyCode === value.key_code) 
-					{
-						var note_octave = current_octave + value.octave_diff;
-						$("#piano [octave|="+note_octave+"] li div[key|="+value.key+"]").removeClass('selected');
-						$("#piano [octave|="+note_octave+"] li span[key|="+value.key+"]").removeClass('selected');
-						key_down[value.key_code] = null;
-					}
-				});
-		    }
-		},this);
-
 		for(index = 0; index <= 5; index++)
 		{
 			$(note).each(function(key, value)
@@ -137,28 +55,105 @@
 			var appendDiv = jQuery($('#piano div[octave|="'+x+'"]')[0].outerHTML);
 			appendDiv.attr('octave', ++octave).insertAfter('#piano div[octave|="'+x+'"]');
 		}
+
+		// generate the key list
+		get_keys();
+		// generate the scale list
 		get_scale();
 
 		$("#piano div li div, #piano div li span").on("mousedown", function(){click_play(this)}); // click the notes to play them
 	});	
 
+function keyboard_init()
+{
+	var key_down = {}; // prevent multiple executions of play_sound when the user holds down a key
+	$(document).on({
+	    "keydown": function(e) 
+	    { 
+			// if the keyboard setting is enabled then we can play the notes
+			if (keyboard_control === true)
+			{
+				// Decrease Octave
+				if (e.keyCode === 90)
+				{
+					if (current_octave > 0)
+					{
+						// decrease the octave value by 1
+						current_octave--;
+						// set the octave list value to the newly changed octave value
+						$('#octave_list').val(current_octave).prop('selected', true);
+					} 
+					else 
+					{
+						return false;
+					}
+				} 
+				
+				// Increase Octave
+				else if (e.keyCode === 88)
+				{
+					if (current_octave < 4)
+					{
+						// increase the octave value by 1
+						current_octave++;
+						// set the octave list value to the newly changed octave value
+						$('#octave_list').val(current_octave).prop('selected', true);
+					}
+					else
+					{
+						return false;
+					}
+				}
+				else 
+				{    			
+					$(note).each(function(k,value) 
+					{
+						if(e.keyCode === value.key_code) 
+						{
+							if (key_down[value.key_code] == null) 
+							{
+	    						var note_octave = (current_octave + value.octave_diff);
+	    						$("#piano [octave|="+note_octave+"] li div[key|="+value.key+"]").addClass('selected');
+	    						$("#piano [octave|="+note_octave+"] li span[key|="+value.key+"]").addClass('selected');
+	    						play_multi_sound(note_octave+value.sound);
+	    						key_down[value.key_code] = true;
+							}
+						}
+					});
+				}
+			}
+		},
+	    "keyup": function(e) 
+	    { 
+			// if the keyboard setting is enabled then we can play the notes
+			if (keyboard_control === true)
+			{
+				$(note).each(function(k,value) 
+				{
+					if (e.keyCode === value.key_code) 
+					{
+						var note_octave = current_octave + value.octave_diff;
+						$("#piano [octave|="+note_octave+"] li div[key|="+value.key+"]").removeClass('selected');
+						$("#piano [octave|="+note_octave+"] li span[key|="+value.key+"]").removeClass('selected');
+						key_down[value.key_code] = null;
+					}
+				});
+			}
+	    }
+	},this);
+}
+
 function click_play(obj_this)
 {
 	var this_octave = $(obj_this).parents('div').attr('octave');
 	var this_key = $(obj_this).attr('key');
-	//console.log(this_octave, this_key);
-
 	
 	$(note).each(function(k, value)
 	{
 		if (value.key === this_key && value.octave_diff === 0)
 		{
-			//console.log(this_octave,value.sound)
 			play_multi_sound(this_octave+value.sound);
 		}
-		//console.log(value);
-		//console.log(note_octave,value.sound);
-		//play_multi_sound(note_octave+value.sound);
 	});
 }
 
@@ -183,6 +178,25 @@ function play_multi_sound(s) {
 		}
 	}
 }	
+
+function get_keys()
+{
+	var key_list = new Array();
+	$(scales).each(function(key,value)
+	{
+		var in_array = $.inArray(value.key,key_list);
+		// add the new key to the list
+		if (in_array === -1)
+		{
+			$('#key_list').append('<option value="'+value.key+'">'+value.key+'</option');
+			key_list.push(value.key);
+		} 
+		// else
+		// {
+		// 	return false;
+		// }
+	});
+}
 
 function get_scale()
 {
@@ -212,21 +226,34 @@ function set_scale()
 		$(value).each(function(key, value)
 		{
 			if (value.name === selected_scale)
-		{
-			$(value.keys).each(function(k,v) 
 			{
-				//$('<div class="scale_dot"></div>').appendTo($('#piano').find("[key|='"+v+"'']"));
-				$('#piano').find("[key|='"+v+"'],[alt-key|='"+v+"']").html('<div class="scale_dot"></div>'); // comma allows us to use either the key or the alt-key values if available
-			});
-		} 
-		else 
-		{
-			return false; // no scale found... for some reason
-		}
-			
+				$(value.keys).each(function(k,v) 
+				{
+					//$('<div class="scale_dot"></div>').appendTo($('#piano').find("[key|='"+v+"'']"));
+					$('#piano').find("[key|='"+v+"'],[alt-key|='"+v+"']").html('<div class="scale_dot"></div>'); // comma allows us to use either the key or the alt-key values if available
+				});
+			} 
+			else 
+			{
+				return false; // no scale found... for some reason
+			}
 		})
 	});
 }
+
+function set_octave() 
+{
+	// set the octave select list to the default value
+	var octave_list_value = $('#octave_list :selected').index();
+	// set the current octave to the newly selected value
+	current_octave = octave_list_value;
+}
+
+function toggle_keyboard()
+{
+	keyboard_control = $('#keyboard_checkbox').is(':checked');
+}
+
 </script>
 <body>
 	<nav class="top-bar">
@@ -270,12 +297,14 @@ function set_scale()
     <br />
 
     <div class="row panel">
+
 		<div class="large-2 columns">
-			<h5>Volume</h5>
-			<div class="range-slider" data-slider="100">
-				<span class="range-slider-handle" aria-valuemin="0" aria-valuemax="100" aria-valuenow="44" style="-webkit-transform: translateX(397.64px); transform: translateX(397.64px);"></span>
-			</div>
+			<h5>Key</h5>
+			<select id="key_list" onchange="set_key(); return false;">
+				<option>None</option>
+			</select>
 		</div>
+
 		<div class="large-2 columns">
 			<h5>Scale</h5>
 			<select id="scale_list" onchange="set_scale(); return false;">
@@ -286,15 +315,39 @@ function set_scale()
 				</optgroup>
 			</select>
 		</div>
+		
 		<div class="large-2 columns">
 			<h5>Keyboard</h5>
-			<a href="#" class="button"><img src="assets/images/keyboard_white.png" /></a>
+			<div class="switch round">
+			  <input id="keyboard_checkbox" type="checkbox" checked="checked" onchange="toggle_keyboard();">
+			  <label for="keyboard_checkbox"></label>
+			</div> 
+		</div>
+		
+		<div class="large-2 columns">
+			<h5>Octave</h5>
+			<select id="octave_list" style="width:40px;" onchange="set_octave(); return false;">
+				<option>0</option>
+				<option>1</option>
+				<option selected>2</option>
+				<option>3</option>
+				<option>4</option>
+			</select>
+		</div>
 
+		<div class="large-2 columns">
+			<h5>Scale Colour</h5>
 		</div>
-		<div class="large-6 columns">
-			stuff goes here
+
+		<div class="large-2 columns">
+			<h5>Volume</h5>
+			<div class="range-slider" data-slider="100">
+				<span class="range-slider-handle" aria-valuemin="0" aria-valuemax="100" aria-valuenow="44" style="-webkit-transform: translateX(397.64px); transform: translateX(397.64px);"></span>
+			</div>
 		</div>
+
 	</div>
+
 <script src="<?php echo site_url('assets'); ?>/js/foundation.min.js"></script>
 <script>
   $(document).foundation();
