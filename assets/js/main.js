82e41e3e1a4fb852ceb4523e3b2271332037f38f
brent-g <brent.godsoe@gmail.com>
2014-12-23 11:30:01 -0500

    Performance tweaks

--- assets/js/main.js
+++ assets/js/main.js
@@ -186,7 +186,7 @@ function play_multi_sound(id) {
 		thistime = new Date();
 		if (audiochannels[a]['finished'] < thistime.getTime()) 
 		{			// is this channel finished?
-			audiochannels[a]['finished'] = thistime.getTime() + $('#sample_'+id).duration*3000;
+			audiochannels[a]['finished'] = thistime.getTime() + $('#sample_'+id).duration*4000;
 			audiochannels[a]['channel'].src = $('#sample_'+id).attr('src');
 			audiochannels[a]['channel'].load();
 			audiochannels[a]['channel'].volume = sound_volume;
